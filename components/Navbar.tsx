ab51c3f993df653fbca80b250d56d11536295267
Marin Ivandic <marin@pragmatic.hr>
2024-10-05 19:12:15 +0200

    add functional hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,16 +4,17 @@ import Link from "next/link";
 import Image from "next/image";
 // import { getDictionary } from "@/lib/utils";
 import { Dictionary, SupportedLanguage } from "@/lib/types/definitions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import LogoBeige from "@/public/logo-beige.svg";
 import LogoDark from "@/public/logo-dark.svg";
 import BarsBeige from "@/public/bars-beige.svg";
 import BarsDark from "@/public/bars-dark.svg";
+import SelectLanguage from "@/components/SelectLanguage";
 
 const Navbar = ({
   t,
-  // lang,
+  lang,
   variant,
 }: {
   t: Dictionary;
@@ -27,6 +28,19 @@ const Navbar = ({
     setToggledMenu((current: boolean) => !current);
   }
 
+  useEffect(() => {
+    if (toggledMenu) {
+      document.body.classList.add("overflow-hidden"); // Disable scrolling
+    } else {
+      document.body.classList.remove("overflow-hidden"); // Enable scrolling
+    }
+
+    // Cleanup function to ensure overflow is reset
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [toggledMenu]);
+
   return (
     <nav
       className={`absolute flex justify-between items-center w-full ${
@@ -37,7 +51,7 @@ const Navbar = ({
           : ""
       } max-w-screen-xl xl:max-w-screen-lg mx-auto py-10 sm:py-5 left-0 right-0 top-0 z-10 ${
         variant === "dark" ? "text-dark-5" : "text-light-3"
-      }`}
+      } ${toggledMenu && "sm:min-h-screen h-0"} sm:flex-col`}
     >
       <div className="sm:w-10/12 grid grid-cols-2 w-full mx-auto max-w-screen-xl xl:max-w-screen-lg sm:grid sm:grid-cols-2">
         <Link href="/">
@@ -127,6 +141,22 @@ const Navbar = ({
             <Link className="block" href="/blog/65e8987edff6423999ac1b11">
               {t.Footer.extras}
             </Link>
+          </li>
+          <li>
+            <div>
+              <div className="mb-2.5">{t.Footer.languageSelection}</div>
+              <SelectLanguage lang={lang} className={"text-dark-5 p-2.5"} />
+            </div>
+          </li>
+          <li>
+            <div>
+              <div className="mb-2.5">{t.NeedHelp.emailAddress}</div>
+              <div>
+                <Link className="underline" href="mailto:info@tst-touristik.de">
+                  info@tst-touristik.de
+                </Link>
+              </div>
+            </div>
           </li>
         </ul>
       </div>
